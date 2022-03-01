633ce6874bbdbe474be0f0ae6a5f361789c7572e
Asim Poptani <asim@poptani.com>
2022-03-01 15:28:03 +0000

    Trying test

--- spheres/test.c
+++ spheres/test.c
@@ -1,20 +1,34 @@
-#include <papi.h>
+// #include <papi.h>
 #include <stdio.h>
+#include <omp.h>
+
+int addNTimes(int n) {
+   __uint128_t i;
+   for (int x = 0; x < n; x++) {
+      i=(x+1)*(i+1);
+      
+   }
+   return i;
+}
 
 int main() {
 
-   static int events[] = { PAPI_TOT_INS, PAPI_L1_DCM };
-       long long counters[2];
+   // static int events[] = { PAPI_TOT_INS, PAPI_L1_DCM };
+   //     long long counters[2];
 
-   PAPI_start_counters(events, 2);
+   // PAPI_start_counters(events, 2);
 
-   int sum = 0;
+   __uint128_t sum = 0;
    int i = 0;
-   for (i = 0; i < 10; i++) {
-      sum += i;
-   }   
+   #pragma omp parallel for reduction(+:sum)
+   for (i = 0; i < 100000; i++) {
+      __uint128_t x = addNTimes(i);
+      printf("Thread %d\n", omp_get_thread_num());
+      sum += x;
+   } 
+   printf("%lld\n", sum);  
    
-   PAPI_stop_counters(counters, 2);
+   // PAPI_stop_counters(counters, 2);
    
-   printf ("L1 data cache misses: %lli\n", counters[0]);
+   // printf ("L1 data cache misses: %lli\n", counters[0]);
 }
