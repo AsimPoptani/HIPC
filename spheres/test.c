3410982a0490ff5bd9dd3fa2866678d6dcf005c1
Asim Poptani <asim@poptani.com>
2022-03-01 15:31:22 +0000

    Added PAPI back

--- spheres/test.c
+++ spheres/test.c
@@ -1,4 +1,4 @@
-// #include <papi.h>
+#include <papi.h>
 #include <stdio.h>
 #include <omp.h>
 
@@ -13,22 +13,22 @@ int addNTimes(int n) {
 
 int main() {
 
-   // static int events[] = { PAPI_TOT_INS, PAPI_L1_DCM };
-   //     long long counters[2];
+   static int events[] = { PAPI_TOT_INS, PAPI_L1_DCM };
+       long long counters[2];
 
-   // PAPI_start_counters(events, 2);
+   PAPI_start_counters(events, 2);
 
    __uint128_t sum = 0;
    int i = 0;
    #pragma omp parallel for reduction(+:sum)
    for (i = 0; i < 100000; i++) {
       __uint128_t x = addNTimes(i);
-      printf("Thread %d\n", omp_get_thread_num());
+      // printf("Thread %d\n", omp_get_thread_num());
       sum += x;
    } 
    printf("%lld\n", sum);  
    
-   // PAPI_stop_counters(counters, 2);
+   PAPI_stop_counters(counters, 2);
    
-   // printf ("L1 data cache misses: %lli\n", counters[0]);
+   printf ("L1 data cache misses: %lli\n", counters[0]);
 }
